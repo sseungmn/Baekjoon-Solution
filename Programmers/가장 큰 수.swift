a702081efa2b5b81657e8a938e4eaffcb826c992
sseungmn <severck12@naver.com>
2022-06-28 19:11:48 +0900

    프로그래머스 레벨별 분류

--- Programmers/가장 큰 수.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-func solution(_ numbers:[Int]) -> String {
-    let ret = numbers
-    .map { String($0) }
-    .sorted { Int($0 + $1)! > Int($1 + $0)! }
-    .joined()
-    return Int(ret) == Optional(0) ? "0" : ret
-}
