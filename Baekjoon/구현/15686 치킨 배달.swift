a778722cca5e932dbc84b1728415c5511a37a23a
sseungmn <severck12@naver.com>
2022-06-29 14:37:57 +0900

    15686 치킨 배달 : 로직 업데이트

--- Baekjoon/구현/15686 치킨 배달.swift
+++ Baekjoon/구현/15686 치킨 배달.swift
@@ -1,23 +1,5 @@
 let dx = [0, 0, 1, -1]
 let dy = [1, -1, 0, 0]
-
-struct Queue<T> {
-  private var enqueue: [T]
-  private var dequeue: [T] = []
-
-  init(_ enqueue: [T] = []) { self.enqueue = enqueue.reversed() }
-
-  var isEmpty: Bool { enqueue.isEmpty && dequeue.isEmpty }
-
-  mutating func push(_ elem: T) { enqueue.append(elem) }
-  mutating func pop() -> T? {
-    if dequeue.isEmpty {
-      dequeue = enqueue.reversed()
-      enqueue.removeAll()
-    }
-    return dequeue.popLast()
-  }
-}
 // 치킨거리 = 집 ~ 가장 가까운 치킨집 사이의 거리
 // 도시의 치킨거리 = 모든 집의 "치킨 거리"의 합
 // 거리 = 맨해튼 거리
@@ -27,28 +9,32 @@ struct Queue<T> {
 // 치킨집을 모두 배열에 담아놓는다.
 // 백트래킹으로 하나씩 제거하면서 치킨거리를 구한다.
 // 깊이 M을 베이스로 한다.
+// --------------------------------------
+// 집 <-> 치킨집 사이의 모든 거리들을 계산해 저장해놓으면
+// 거리를 여러번 계산하는 낭비를 줄일 수 있다.
 
 typealias Coordinate = (x: Int, y: Int)
 var line = readLine()!.split(separator: " ").map { Int($0)! }
 let n = line[0], m = line[1]
 
-var board = [[Int]](repeating: [Int](repeating: 0, count: n), count: n)
 var stores = [Coordinate](), houses = [Coordinate]()
 stores.reserveCapacity(13); houses.reserveCapacity(100)
 
 for y in 0..<n {
   line = readLine()!.split(separator: " ").map { Int($0)! }
   for x in 0..<n {
-    board[y][x] = line[x]
-    if line[x] == 2 { stores.append((x, y)); board[y][x] = 0 }
+    if line[x] == 2 { stores.append((x, y)) }
     else if line[x] == 1 { houses.append((x, y)) }
   }
 }
 
 func recursion(_ picked: [Coordinate], _ index: Int) {
-  if picked.count == m || index == stores.count {
-    let cityDist = dist(picked)
-    ret = cityDist == 0 ? ret : min(ret, dist(picked))
+  if picked.count == m {
+    ret = min(ret, dist(picked))
+    return
+  }
+  if index == stores.count {
+    if picked.count > 0 { ret = min(ret, dist(picked)) }
     return
   }
 
@@ -68,8 +54,6 @@ func dist(_ pickedStore: [Coordinate]) -> Int {
     }
     cityDist += chickenDist
   }
-  print("=============================")
-  print("cityDist(\(cityDist)) -> pickedStore : \(pickedStore)")
   return cityDist
 }
 
